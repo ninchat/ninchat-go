d73bccdcf88a776ee7f09ae7c2ea54e831ff92b5
Timo Savola <timo@ninchat.com>
2015-10-12 14:01:20 +0300

    ninchatapi: documentation clarification

---
+++ ninchatapi/params.go
@@ -0,0 +1,19 @@
+package ninchatapi
+
+// AppendStrings duplicates the source slice while unwrapping the elements.
+func AppendStrings(target []string, source []interface{}) []string {
+	if source != nil {
+		if target == nil || cap(target) < len(target)+len(source) {
+			t := make([]string, len(target), len(target)+len(source))
+			copy(t, target)
+			target = t
+		}
+
+		for _, x := range source {
+			y, _ := x.(string)
+			target = append(target, y)
+		}
+	}
+
+	return target
+}
