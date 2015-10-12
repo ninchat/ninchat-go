e2fa40625b5cb1d9b491e36edec57c93969b43d4
Timo Savola <timo@ninchat.com>
2015-10-12 18:37:01 +0300

    ninchatmessage: Content factory functions

---
+++ ninchatmessage/factory.go
@@ -0,0 +1,24 @@
+package ninchatmessage
+
+// Factories contains default constructors for all known message types.
+var Factories = map[string]func() Content{
+	AccessInfoType:  func() Content { return new(AccessInfo) },
+	ChannelInfoType: func() Content { return new(ChannelInfo) },
+	JoinInfoType:    func() Content { return new(JoinInfo) },
+	LinkType:        func() Content { return new(Link) },
+	MemberInfoType:  func() Content { return new(MemberInfo) },
+	NoticeType:      func() Content { return new(Notice) },
+	PartInfoType:    func() Content { return new(PartInfo) },
+	TextType:        func() Content { return new(Text) },
+	UserInfoType:    func() Content { return new(UserInfo) },
+}
+
+// NewContent creates a message object from a payload.  nil is returned if the
+// type is unknown.
+func NewContent(messageType string, payload [][]byte) (c Content, err error) {
+	if f := Factories[messageType]; f != nil {
+		c = f()
+		err = c.Unmarshal(payload)
+	}
+	return
+}
