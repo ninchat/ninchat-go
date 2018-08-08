129b8108264c6f95d076fdf2cab0142dd9e1b6ec
Timo Savola <timo@ninchat.com>
2018-08-08 16:11:22 +0300

    mobile: send: use GetId

--- mobile/client.go
+++ mobile/client.go
@@ -296,9 +296,7 @@ func (s *Session) Send(params *Props, payload *Payload) (actionId int64, err err
 	}
 	err = s.s.Send(action)
 	if err == nil {
-		if x, found := action.Params["action_id"]; found && x != nil {
-			actionId = x.(int64)
-		}
+		actionId, _ = action.GetId()
 	}
 	return
 }
